495a272a2bd2af045244502352302d36666df54d
Vishwa Perera <pereravishwa061@gmail.com>
2019-12-30 04:14:40 -0500

    added p5 integration

--- pathfinding-algorithms/src/components/grid/Grid.js
+++ pathfinding-algorithms/src/components/grid/Grid.js
@@ -1,12 +1,39 @@
 import React, { Component } from "react"
+import p5 from "p5"
+import P5Wrapper from "react-p5-wrapper"
 
 class Grid extends Component {
     render() {
         return (
             <div className="grid">
+                <P5Wrapper sketch={sketch} />
             </div>
         )
     }
 }
+
+function sketch (p) {
+    let rotation = 0;
+  
+    p.setup = function () {
+      p.createCanvas(600, 400, p.WEBGL);
+    };
+  
+    p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
+      if (props.rotation){
+        rotation = props.rotation * Math.PI / 180;
+      }
+    };
+  
+    p.draw = function () {
+      p.background(100);
+      p.normalMaterial();
+      p.noStroke();
+      p.push();
+      p.rotateY(rotation);
+      p.box(100);
+      p.pop();
+    };
+  };
 
 export default Grid
